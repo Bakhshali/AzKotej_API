f92a93477d47bed199e85d585912bfd88c5286e6
Bakhshali <tu78rfrfu@code.edu.az>
2023-08-16 09:15:36 +0400

    updated

--- src/models/home.js
+++ src/models/home.js
@@ -1,13 +1,38 @@
 const { default: mongoose } = require("mongoose")
 
 const homeScheme = new mongoose.Schema({
-    name: String,
-    price:Number,
-    address:String,
-    phone:Number,
-    area:Number,
-    latitude:Number,
-    longtitude:Number,
+    name: {
+        type:String,
+        required:true
+    },
+    price: {
+        type:Number,
+        min:0
+    },
+    address: {
+        type:String,
+        required:true
+    },
+    phone: {
+        type:Number
+    },
+    area: {
+        type:Number,
+        min:0
+    },
+    latitude: {
+        type:Number
+    },
+    longtitude: {
+        type:Number
+    },
+    photos:{
+        type:[String]
+    },
+    description:{
+        type:String,
+        required:true
+    },
     createdAt: { type: Date, default: Date.now }
 
 })
